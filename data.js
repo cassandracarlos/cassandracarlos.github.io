2ab36fa22f98006741807233907a2595d3b68c2b
codingcass <codingcass@gmail.com>
2023-07-31 19:05:17 -0700

    what website

--- data.js
+++
@@ -1,4 +0,0 @@
-var array = [['first1','first2'],['second1','second2'],['thf','tf'],['fourth1','fourth2'],['fifth1','fifth2']];
-
-document.getElementById('placeholder-1').innerHTML = array[2][0];
-document.getElementById('placeholder-2').innerHTML = array[2][1];
