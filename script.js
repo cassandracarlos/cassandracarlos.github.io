2dcb5f4811f3f05a350baefb5dd9f8413f6083a1
Cass <codingcass@gmail.com>
2023-08-24 14:43:35 -0700

    Simplify

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const links = document.querySelector('.links');
-
-hamburger.addEventListener('click', () => {
-    links.classList.toggle('active');
-});
